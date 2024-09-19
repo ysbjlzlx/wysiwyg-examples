7d379847849cc09c9b6b1f196b4e747199bb70ca
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-09-19 23:03:24 +0800

    feat: add clsx

--- src/app/ant-design/md-editor/page.tsx
+++ src/app/ant-design/md-editor/page.tsx
@@ -6,7 +6,13 @@ import { MarkdownEditor, } from '@ant-design/md-editor';
 const Page:FC = () => {
     const defaultValue = `
 # @ant-design/md-editor
-        
+
+* [https://md-to-json-schema.antdigital.dev/](https://md-to-json-schema.antdigital.dev/)
+* https://github.com/ant-design/md-editor
+* https://www.npmjs.com/package/@ant-design/md-editor
+
+## 安装
+
 \`\`\`bash
 pnpm add @ant-design/md-editor
 \`\`\`
