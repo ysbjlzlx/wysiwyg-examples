1a238c5e0462c8f712f6605e0ec1701566b97804
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-09-19 23:01:08 +0800

    feat: add clsx

--- src/app/ant-design/md-editor/page.tsx
+++ src/app/ant-design/md-editor/page.tsx
@@ -1,6 +1,27 @@
-const Page = () => {
+'use client';
+
+import {FC} from "react";
+import { MarkdownEditor, } from '@ant-design/md-editor';
+
+const Page:FC = () => {
+    const defaultValue = `
+# @ant-design/md-editor
+        
+\`\`\`bash
+pnpm add @ant-design/md-editor
+\`\`\`
+  
+        `;
     return <div>
-        @ant-design/md-editor
+        <MarkdownEditor
+            width={'100vw'}
+            height={'100vh'}
+            reportMode
+            initValue={defaultValue}
+            onChange={(e, s) => {
+                console.log('onChange', e, s);
+            }}
+        />
     </div>
 }
 export default Page;
